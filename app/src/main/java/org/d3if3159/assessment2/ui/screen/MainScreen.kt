bd5f2ed9f98ed5cb2c1707ebdf304f965f1f2655
Mariana <marmar.smsr@gmail.com>
2024-05-04 01:20:01 +0700

    Meminta Input Data dari Pengguna

--- app/src/main/java/org/d3if3159/assessment2/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3159/assessment2/ui/screen/MainScreen.kt
@@ -1,18 +1,29 @@
 package org.d3if3159.assessment2.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.d3if3159.assessment2.R
@@ -40,10 +51,42 @@ fun MainScreen(){
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize().padding(16.dp)
-    ) {
+    var judul by remember { mutableStateOf("") }
+    var pencipta by remember { mutableStateOf("") }
 
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.app_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = judul,
+            onValueChange = { judul = it},
+            label = { Text(text = stringResource(id = R.string.judul))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = pencipta,
+            onValueChange = { pencipta = it},
+            label = { Text(text = stringResource(id = R.string.pencipta))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
