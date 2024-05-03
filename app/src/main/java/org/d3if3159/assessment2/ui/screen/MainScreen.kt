0e76a6a4df0f9f7345b3df80e54b3ef5a67d553c
Mariana <marmar.smsr@gmail.com>
2024-05-04 01:55:35 +0700

    Memberi Pilihan Kepada Pengguna

--- app/src/main/java/org/d3if3159/assessment2/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3159/assessment2/ui/screen/MainScreen.kt
@@ -1,15 +1,23 @@
 package org.d3if3159.assessment2.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -19,9 +27,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,10 +63,19 @@ fun MainScreen(){
 fun ScreenContent(modifier: Modifier) {
     var judul by remember { mutableStateOf("") }
     var pencipta by remember { mutableStateOf("") }
+    val radioOptions = listOf(
+        stringResource(id = R.string.pop),
+        stringResource(id = R.string.jazz),
+        stringResource(id = R.string.klasik),
+        stringResource(id = R.string.rock),
+        stringResource(id = R.string.hiphop)
+    )
+    var genre by remember { mutableStateOf(radioOptions[0]) }
 
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
@@ -86,6 +105,40 @@ fun ScreenContent(modifier: Modifier) {
                 imeAction = ImeAction.Next
             ),
             modifier = Modifier.fillMaxWidth()
+        )
+        Column(
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+        ) {
+            radioOptions.forEach { text ->
+                GenreOption(
+                    label = text,
+                    isSelected = genre == text,
+                    modifier = Modifier
+                        .selectable(
+                            selected = genre == text,
+                            onClick = { genre = text },
+                            role = Role.RadioButton
+                        )
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun GenreOption(label: String, isSelected: Boolean, modifier: Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = isSelected, onClick = null)
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 10.dp)
         )
     }
 }
